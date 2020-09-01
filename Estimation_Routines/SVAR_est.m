266e043ec1d0eb461c9f0db60d6790e72f013a3c
mikkelpm <mikkel.p.m@gmail.com>
2020-09-01 19:45:48 -0400

    Also revert sample eigenvalue calculation

--- Estimation_Routines/SVAR_est.m
+++ Estimation_Routines/SVAR_est.m
@@ -71,7 +71,7 @@ IRF = IRF';
 nv = size(Y,2);
 companion_form = diag(ones(1, nv*(nlags-1)), -nv);
 companion_form(1:nv,:) = reshape(By,[nv, nv*nlags]);
-largest_root = abs(eigs(companion_form,1));
+largest_root = max(abs(eig(companion_form)));
 
 % estimate LM-stat to examine VAR(p) fit
 
