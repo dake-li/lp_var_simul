176f6abed4ad98ff5a5885f765b0b4a08d414771
mikkelpm <mikkel.p.m@gmail.com>
2021-02-05 17:55:16 -0500

    Hatched choice plot using matplotlib in Python via Jupyter Notebooks + resolve ties in favor of SVAR

--- DFM/Reporting/run_plot_tradeoff.m
+++ DFM/Reporting/run_plot_tradeoff.m
@@ -32,7 +32,7 @@ settings_shared;
 
 % bias weight grid
 
-n_weight    = 10001;
+n_weight    = 1001;
 weight_grid = linspace(1,0,n_weight)';
 
 % reference method (should have something here on this being LP...)
@@ -151,8 +151,11 @@ for nf=1:length(lags_folders) % For each folder...
         for i_weight = 1:n_weight
             loss_all = weight_grid(i_weight) * the_BIAS2rel + (1-weight_grid(i_weight)) * the_VCErel;
             loss_all = squeeze(mean(loss_all,2));
+            loss_all(:,2:end) = loss_all(:,2:end) + sqrt(eps);
             [~,choice_raw(i_weight,:)] = min(loss_all,[],2);
         end
+        
+        writematrix([0 horzs; weight_grid(:) choice_raw], fullfile(output_folder, 'tradeoff_best.csv')); % Save to file
         
         plot_choice(choice_raw, lines, horzs, weight_grid, methods_select{ne}, ...
                 strjoin({exper_plotname, ': Best Procedure'}), methods_names_plot, 1, font_size);
